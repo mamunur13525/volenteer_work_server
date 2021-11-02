373ad37190a51cceab3da64110e37613ffdc508e
mamun5 <mamun@w3dev.email>
2021-11-02 19:07:14 +0600

    doen

--- index.js
+++ index.js
@@ -1,83 +1,66 @@
-
-const express = require('express')
-const app = express()
-const port = 5050
-const MongoClient = require('mongodb').MongoClient;
-const bodyPersar = require('body-parser');
-const cors = require('cors');
-const { ObjectId } = require('mongodb').ObjectId;
-require('dotenv').config()
-
-const uri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0.mde5k.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
-
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { ObjectId } = require("mongodb");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://creativeAgencyUser:${process.env.DB_PASS}@creativeagency.vucvc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 app.use(cors());
-app.use(bodyPersar.json());
+app.use(bodyParser.json());
 
-
-
-/* Register Collection Database */
-client.connect(err => {
-  const registerCollection = client.db("valunteerNnetwork").collection("registerValenteer");
-    
-  app.get('/',(req, res)=>{
-    res.send('heroku is working')
-  })
-  app.post('/addregister',(req, res)=>{
-    const register = req.body;
-    registerCollection.insertOne(register)
-    .then(result => {
-      res.send(result)
- 
-     
-    })
-
-  })
-
-
- 
-
-  app.get('/registerUser/:email',(req, res)=>{
- 
-    registerCollection.find({email:req.params.email})
-    .toArray((err,documents)=>{
-      res.send(documents)
-     
-    })
-  })
-
-
-  app.delete('/deleteregister/:id',(req, res)=>{
-    registerCollection.deleteOne({_id: ObjectId(req.params.id)})
-      .then(result => {
-        res.send(result.deletedCount> 0)
-        
-      })
-   
-  })
-
- 
-
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+client.connect((err) => {
+  const registerCollection = client
+    .db("voleenter")
+    .collection("subscribe_voleenter");
+  const valunteerCollection = client
+    .db("voleenter")
+    .collection("voleenter_collection");
 
-/* Register Collection Database */
-client.connect(err => {
-    const valunteerCollection = client.db("valunteerNnetwork").collection("allValunteer");
-      
-      app.get('/allValunteer',(req, res) => {
-      
-          valunteerCollection.find({})
-          .toArray((err, documents)=>{
-              res.send(documents)
-           })
-      })
-  
+  app.post("/addregister", (req, res) => {
+    const register = req.body;
+    registerCollection.insertOne(register).then((result) => {
+      res.send(result);
+    });
   });
+
+  app.get("/registerUser/:email", (req, res) => {
+    console.log(req.params.email);
+    registerCollection
+      .find({ email: req.params.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
   
 
+  app.delete("/deleteregister/:id", (req, res) => {
+    registerCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
 
+  /* Register Collection Database */
+  app.get("/allValunteer", (req, res) => {
+    valunteerCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
+app.get("/", (req, res) => {
+  res.send("Hello world It's working");
+});
 
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server running ${process.env.PORT || port}`)
+);
